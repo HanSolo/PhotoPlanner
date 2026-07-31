0f468bba040828ff691189d976b4a63fd9651c0a
Gerrit Grunwald <han.solo@mac.com>
2026-07-31 14:45:07 +0200

    Some cosmetics

--- PhotoPlanner/views/SearchView.swift
+++ PhotoPlanner/views/SearchView.swift
@@ -31,51 +31,38 @@ struct SearchView: View {
                 }
                 .buttonStyle(.glass)
                 .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                Form {
-                    Section(header: Text("")) {
-                        ZStack(alignment: .trailing) {
-                            TextField("Search", text: $locationService.queryFragment)
-                                .disableAutocorrection(true)
-                                .font(Constants.REGULAR_FONT_20)
-                            if locationService.status == .isSearching {
-                                Image(systemName: "clock")
-                            }
-                        }
-                    }
-                    Section {
-                        List {
-                            ForEach(locationService.searchResults, id: \.self) { completionResult in
-                                let mapItem = completionResult.value(forKey: "_mapItem") as? MKMapItem
-                                if mapItem != nil {
-                                    let address  : Address                = Address(mapItem: mapItem!)
-                                    let location : CLLocationCoordinate2D = address.location()
-                                    
-                                    HStack(spacing: .some(Constants.IS_IPAD ? 30 : 15)) {
-                                        Button {
-                                        } label: {
-                                            Image(systemName: "map.circle.fill")
-                                                .background(.clear)
-                                                .font(.system(size: 22))
-                                        }
-                                        .highPriorityGesture(TapGesture().onEnded {
-                                            functio(location)
-                                            dismiss()
-                                        })
-                                        Text("\(Helper.formatAddress(name: completionResult.title, address: address))")
-                                            .font(Constants.REGULAR_FONT_14)
-                                    }
-                                    
+                                
+                List {
+                    ForEach(locationService.searchResults, id: \.self) { completionResult in
+                        let mapItem = completionResult.value(forKey: "_mapItem") as? MKMapItem
+                        if mapItem != nil {
+                            let address  : Address                = Address(mapItem: mapItem!)
+                            let location : CLLocationCoordinate2D = address.location()
+                            
+                            HStack(spacing: .some(Constants.IS_IPAD ? 30 : 15)) {
+                                Button {
+                                } label: {
+                                    Image(systemName: "map.circle.fill")
+                                        .background(.clear)
+                                        .font(.system(size: 22))
                                 }
+                                .highPriorityGesture(TapGesture().onEnded {
+                                    functio(location)
+                                    dismiss()
+                                })
+                                Text("\(Helper.formatAddress(name: completionResult.title, address: address))")
+                                    .font(Constants.REGULAR_FONT_14)
                             }
+                            
                         }
-                        .scrollContentBackground(.hidden)
                     }
                 }
+                .scrollContentBackground(.hidden)
             }
             .backgroundStyle(.thinMaterial)
             .padding(16)
         }
+        .searchable(text: $locationService.queryFragment, prompt: "Search for a location")
         .background(Color(.systemGroupedBackground).opacity(0.01))
         .backgroundStyle(.thinMaterial)
         .presentationDragIndicator(.visible)
